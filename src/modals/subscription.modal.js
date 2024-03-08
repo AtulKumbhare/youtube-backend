ffb24080248005322574e8467e301f8d6153fb93
Atul Kumbhare <kumbhare76@gmail.com>
2024-03-08 20:09:02 +0530

    fixed subscription schema

--- src/modals/subscription.modal.js
+++ src/modals/subscription.modal.js
@@ -3,11 +3,11 @@ import { Schema, model, Types } from 'mongoose';
 const subscriptionSchema = new Schema(
 	{
 		subscriber: {
-			type: [Types.ObjectId], //User who is subscribing
+			type: Types.ObjectId, //User who is subscribing
 			ref: 'User',
 		},
 		channel: {
-			type: [Types.ObjectId], //User who has a channel which get subscribed by other users
+			type: Types.ObjectId, //User who has a channel which get subscribed by other users
 			ref: 'User',
 		},
 	},
